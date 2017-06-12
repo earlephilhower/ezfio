37748900d8dff3c692e563989c1cba91c9161526
Earle Philhower <earlephilhower@yahoo.com>
2017-06-12 11:17:19 -0700

    Fix startup to work in paths with spaces
    The EZFIO.BAT script needs to invoke a couple levels of PowerShell
    to start our script, and if quoting rules are not carefully followed
    this will fail if there is a space anywhere in the path to the script.
    Rewrote and tested the bouncer batch file using a directory including
    a space.  Fixes issue 14 and issue 8.

--- ezfio.bat
+++ ezfio.bat
@@ -1,1 +1,9 @@
-powershell -command "start -verb runas powershell.exe -windowstyle hidden %cd%\ezfio.ps1"
+@echo off
+REM Start EZFIO.PS1 in an elevated PowerShell interpreter.
+REM Here be dragons.
+REM First start a standard powershell and use it's Start-Process cmdlet
+REM to start *another* powershell, this one as administrator, to interpret
+REM the script.  Care must be taken to properly quote the path to the script.
+
+set GO='%cd%\ezfio.ps1'
+powershell -Command "$p = new-object System.Diagnostics.ProcessStartInfo 'PowerShell'; $p.Arguments = {-WindowStyle hidden -Command ". %GO%"}; $p.Verb = 'RunAs'; [System.Diagnostics.Process]::Start($p) | out-null;"
